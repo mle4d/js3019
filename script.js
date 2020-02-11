28934a78c9ed8b711929a3029519124f6b15d446
emily <mle4d@me.com>
2020-02-10 21:46:09 -0800

    took out the red effect & rbg split effect because I'm stuck & need to start over fresh (eyeroll emoji)

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
+
 function getVideo() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
@@ -29,6 +30,9 @@ function paintToCanvas() {
   }, 0);
 }
 
+    
+
+
 function takePhoto() {
   snap.currentTime = 0;
   snap.play();
@@ -40,23 +44,6 @@ function takePhoto() {
   strip.insertBefore(link, strip.firstChild);
 }
 
-function redEffect(pixels) {
-  for (let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
-  }
-  return pixels;
-}
-
-function rgbSplit(pixels) {
-  for (let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i + 0] = pixels.data[i + 2]^255; // RED
-    pixels.data[i + 3] = pixels.data[i + 1]^255; // GREEN
-    pixels.data[i + 2] = pixels.data[i + 8] ^255; // Blue
-  }
-  return pixels;
-}
 
 function greenScreen(pixels) {
   const levels = {};
